287f0ab655ae828decbef3e84b9170f8a8ba2861
nati3514 <natiage3514@gmail.com>
2023-11-30 20:50:24 +0300

    Update product model to use file system to store products (generated by blackbox.ai )

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,9 +7,63 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        try {
+          products = JSON.parse(fileContent);
+        } catch (parseError) {
+          console.error("Error parsing JSON:", parseError);
+        }
+      }
+
+      products.push(this);
+
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          console.error("Error writing to file:", err);
+        }
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        if (err.code === "ENOENT") {
+          // File doesn't exist, create an empty array
+          fs.writeFile(p, "[]", (err) => {
+            if (err) {
+              console.error("Error creating file:", err);
+            }
+          });
+          cb([]); // Return an empty array
+        } else {
+          console.error("Error reading file:", err);
+          cb([]);
+        }
+      } else {
+        try {
+          const products = JSON.parse(fileContent);
+          cb(products);
+        } catch (parseError) {
+          console.error("Error parsing JSON:", parseError);
+          cb([]);
+        }
+      }
+    });
   }
 };
