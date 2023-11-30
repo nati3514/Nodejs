cb068e2893fabd6ca0220d4a17a7ad69f0255171
nati3514 <natiage3514@gmail.com>
2023-11-30 11:35:04 +0300

    Working with EJS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -16,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found " });
-});
+  res.status(404).render("404", { path: req.path, pageTitle: "Page Not Found" });
+})
 
 app.listen(3000);
